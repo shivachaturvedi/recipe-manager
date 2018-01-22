e862732a14f53a45bab352cb9b6d7874368cf963
shiva.chaturvedi@gmail.com <shiva.chaturvedi@gmail.com>
2018-01-22 04:18:18 +0000

    Changed Carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,26 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    }
-    
-    config.fog_directory = ENV['S3_BUCKET']
-  
-  end
+  config.storage    = :aws
+  config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+  config.aws_acl    = 'public-read'
+
+  # Optionally define an asset host for configurations that are fronted by a
+  # content host, such as CloudFront.
+  config.asset_host = 'http://example.com'
+
+  # The maximum period for authenticated_urls is only 7 days.
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+  # Set custom options such as cache control to leverage browser caching
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
+  }
+
+  config.aws_credentials = {
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            ENV.fetch('AWS_REGION') # Required
+  }
+end
 end
