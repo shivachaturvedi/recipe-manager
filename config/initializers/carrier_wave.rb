15275dc0f76babd7edeef6ca6d79af31c1401641
shiva.chaturvedi@gmail.com <shiva.chaturvedi@gmail.com>
2018-01-22 05:09:11 +0000

    Changed ENV NAME

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,23 +1,18 @@
 if Rails.env.production?
-  CarrierWave.configure do |config|
-  
-  config.aws_bucket = ENV.fetch('S3_BUCKET')
-  config.aws_acl    = 'public-read'
 
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'  
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region => ENV['AWS_REGION']
+    }
+    config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET']
+    config.asset_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET']}"
+  end
 
-  # Set custom options such as cache control to leverage browser caching
-  config.aws_attributes = {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  }
-
-  config.aws_credentials = {
-    access_key_id:     ENV.fetch('S3_ACCESS_KEY'),
-    secret_access_key: ENV.fetch('S3_SECRET_KEY'),
-    region:            ENV.fetch('AWS_REGION') # Required
-  }
-  config.storage    = :aws
 end
-end
+
+
